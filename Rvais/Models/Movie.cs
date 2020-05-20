633bd65718511803ae0c80a83126fc37a598852f
iantanasijevic <iantanasijevic14@raf.rs>
2020-05-20 18:59:10 +0200

    mongoDb

---
+++ Rvais/Models/Movie.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Rvais.Models
+{
+    public class Movie
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("Name")]
+        public string MovieName { get; set; }
+
+        public string Plot { get; set; }
+
+        public int Year { get; set; }
+    }
+}
---
+++ Rvais/Models/RvaisDatabaseSettings.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Rvais.Models;
+
+namespace Rvais.Models
+{
+    public class RvaisDatabaseSettings : IRvaisDatabaseSettings
+    {
+        public string CollectionName { get; set; }
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+    }
+
+    public interface IRvaisDatabaseSettings
+    {
+        string CollectionName { get; set; }
+        string ConnectionString { get; set; }
+        string DatabaseName { get; set; }
+    }
+}
